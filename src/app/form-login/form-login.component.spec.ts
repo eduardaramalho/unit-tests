42173289396eb48ba54ef7c1550f67dab4b97b31
Eduarda Ramalho <eduardacns.ramalho@gmail.com>
2023-08-28 15:31:55 -0300

    feat: tests on form login

--- src/app/form-login/form-login.component.spec.ts
+++ src/app/form-login/form-login.component.spec.ts
@@ -4,16 +4,18 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpService } from '../service/http.service';
 
 import { FormLoginComponent } from './form-login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NumberFormatStyle } from '@angular/common';
 
-describe('FormLoginComponent', () => {
+fdescribe('FormLoginComponent', () => {
   let component: FormLoginComponent;
   let fixture: ComponentFixture<FormLoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormLoginComponent ],
-    })
-    .compileComponents();
+      declarations: [FormLoginComponent],
+      imports: [ReactiveFormsModule, HttpClientModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FormLoginComponent);
     component = fixture.componentInstance;
@@ -22,5 +24,41 @@ describe('FormLoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return is form is invalid', () => {
+    const result = component.isValidForm();
+
+    expect(result).toBe(false);
+  });
+
+  it('should return is form is valid', () => {
+    component.form.controls['email'].setValue('eduarda@gmail.com');
+    component.form.controls['password'].setValue('123456');
+
+    const result = component.isValidForm();
+
+    expect(result).toBe(true);
+  });
+
+  it('should turn button off when form is invalid', () => {
+    const button = fixture.debugElement;
+
+    expect(button.nativeElement.querySelector('.btn-login').disabled).toBe(
+      true
+    );
+  });
+
+  it('should turn button on when form is valid', () => {
+    component.form.controls['email'].setValue('eduarda@gmail.com');
+    component.form.controls['password'].setValue('123456');
+
+    const button = fixture.debugElement;
+
+    fixture.detectChanges();
+
+    expect(button.nativeElement.querySelector('.btn-login').disabled).toBe(
+      false
+    );
   });
 });
